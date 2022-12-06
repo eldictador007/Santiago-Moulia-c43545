c25ae23fabad410b00aa6d362a2c17b79ecab507
eldictador007 <chantilli@protonmail.ch>
2022-12-06 18:44:02 -0300

    volvemos a empezar con detail

--- mi-App/src/componentes/itemDetailContainer/itemDetailCont.jsx
+++ mi-App/src/componentes/itemDetailContainer/itemDetailCont.jsx
@@ -6,14 +6,7 @@ import { useEffect, useState } from "react"
 export const ItemDetailContainer=()=>{
     const [item,setItem]=useState([])
     const[loading,setLoading]=useState(true)
-    const useFetch= async()=>{
-        try{
-            const resp= await gFetch([0])
-            const respJson= await resp.json()
-            setItem(respJson)
-        }catch(err){console.log(err)}finally{()=> setLoading(false)}
-    }
-    useEffect(()=>{useFetch()})
+    
     return <>
     <Item product={item}/>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse dolorum quidem itaque a tempora sint corrupti ullam tenetur molestias dignissimos sit, quasi aut, inventore exercitationem accusamus alias, assumenda repudiandae delectus.</p>
