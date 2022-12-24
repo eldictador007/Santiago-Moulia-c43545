4470f86761672629e04d1d642b017087ffb84a2a
eldictador007 <chantilli@protonmail.ch>
2022-12-24 12:29:10 -0300

    reparando

--- mi-App/src/componentes/ItemListContainer/item.jsx
+++ mi-App/src/componentes/ItemListContainer/item.jsx
@@ -7,7 +7,7 @@ export const Item=({product})=>{
                 return    <div style={{ margin: 50, maxWidth:450}} className='col-sm-8' >                                                        
                                                     <div className="card text-center w-100 mt-5" >
                                                         <div className="card-header ">
-                                                            {`${product.name} - ${product.category}`}
+                                                            {`${product.name} - ${product.cat}`}
                                                         </div>
                                                         <div className="card-body ">
                                                             <img src={product.pic} alt={product.name} className='w-50' />
